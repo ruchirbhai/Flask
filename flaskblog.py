b952db6f57216fddd36bb122687558b850edf622
ruchirbhai <ruchirbhai@gmail.com>
2020-03-19 18:14:49 -0700

    Flask Blog initial commit
    basic homepage and about page

---
+++ flaskblog.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+#app.route are decorators will handle complicated backend stuff
+@app.route('/')
+def hello_world():
+    return '<h1>Home Page</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>About Page</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
