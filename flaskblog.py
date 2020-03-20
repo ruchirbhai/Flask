439a225e97b5933992cca75aa76395a3206d1d63
ruchirbhai <ruchirbhai@gmail.com>
2020-03-19 18:40:27 -0700

    Add HTML templates
    Add home and about HTML templates and display blog posts on the home page with for and if else loops

--- flaskblog.py
+++ flaskblog.py
@@ -1,19 +1,33 @@
 # following Corey Schafer Python Flask Tutorials
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
+posts = [
+    {
+        'author': 'Ruchir Sutaria',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'March 19, 2020'
+    },
+    {
+        'author': 'Corey schafer',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'March 19, 2020'
+    },
+]
 # app.route are decorators will handle complicated backend stuff
 @app.route('/')
 @app.route('/home')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html',posts=posts)
 
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
